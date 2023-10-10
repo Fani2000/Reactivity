ab10df0b635145dd70782a933a1662e0d324065c
fkeorapetse <fkeorapetse@quickloc8.co.za>
2023-10-10 04:52:18 +0200

    Updated the Activities UI and also added grud operations for the local activities

---
+++ client-app/src/app/layout/Navbar.tsx
@@ -0,0 +1,29 @@
+import { Button, Container, Menu } from "semantic-ui-react";
+
+interface Props {
+  openForm: (id?: string) => void;
+}
+
+const Navbar = ({ openForm }: Props) => {
+  return (
+    <Menu inverted fixed="top">
+      <Container>
+        <Menu.Item header>
+          <img src="/assets/logo.png" alt="logo" style={{ marginRight: 10 }} />
+          Reactivities
+        </Menu.Item>
+
+        <Menu.Item name="Activities"></Menu.Item>
+        <Menu.Item>
+          <Button
+            positive
+            content="Create Activity"
+            onClick={() => openForm()}
+          />
+        </Menu.Item>
+      </Container>
+    </Menu>
+  );
+};
+
+export default Navbar;
